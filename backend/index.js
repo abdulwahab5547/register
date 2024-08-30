b30864a616611ce39a85550503eda59a7931be1f
Abdul Wahab Asif <136114691+abdulwahab5547@users.noreply.github.com>
2024-08-30 16:32:05 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,21 +4,22 @@ import express from 'express';
 // Creating an express application
 const app = express()
 
+
 // Importing and Configuring dotenv
 import dotenv from 'dotenv';
 dotenv.config();
 
 // Importing cors
 import cors from 'cors';
+app.use(cors({ origin: '*' }));
+// // Enable CORS for all origins
+// const corsOptions = {
+//   origin: 'https://register-blush.vercel.app', // Replace with your frontend's URL
+//   methods: 'GET,POST,PUT,DELETE',
+//   allowedHeaders: 'Content-Type,Authorization'
+// };
 
-// Enable CORS for all origins
-const corsOptions = {
-  origin: 'https://register-blush.vercel.app', // Replace with your frontend's URL
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization'
-};
-
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.get('/data', (req, res) => {
     res.json({ message: 'Backend connected properly.' });
